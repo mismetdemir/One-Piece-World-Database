313ad1afca99266b76e8fd6f651fc8685be3baf5
mismetdemir <mismetdemir@hotmail.com>
2026-05-16 16:19:36 +0300

    Added authorization routes and connected it to server

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
+const authRoutes = require("./routes/authRoutes");
 const characterRoutes = require("./routes/characterRoutes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.use("/api/auth", authRoutes);
 app.use("/api/characters", characterRoutes);
 app.listen(PORT, () => {console.log(`Server is running on port: ${PORT}`)});
