5708a49164ee5631eea002fb3d33cfae46101a91
mismetdemir <mismetdemir@hotmail.com>
2026-05-16 15:15:18 +0300

    Added email field to users

--- backend/database/db.js
+++ backend/database/db.js
@@ -25,6 +25,7 @@ db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL UNIQUE,
         password TEXT NOT NULL,
         role TEXT NOT NULL DEFAULT 'user' CHECK(role IN ('user', 'editor', 'admin')),
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP
