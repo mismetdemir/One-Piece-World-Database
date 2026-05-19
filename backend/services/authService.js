747f3d7a17f327ff0f1820e274a901d146372649
mismetdemir <mismetdemir@hotmail.com>
2026-05-19 12:00:05 +0300

    Added user registration

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -18,6 +18,100 @@ function findUserByUsernameOrEmail(identifier) {
     });
 }
 
+function findExistingUser(username, email) {
+    return new Promise((resolve, reject) => {
+        db.get(
+            "SELECT * FROM users WHERE username = ? OR email = =,?",
+            [username, email],
+            (err, user) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(user);
+                }
+            }
+        );
+    });
+}
+
+function insertUser(username, email, hashedPassword) {
+    return new Promise((resolve, reject) => {
+        db.run(
+            "INSER INTO users (username, email, password, role) VALUES (?, ?, ?, ?)",
+            [username, email, hashedPassword, "user"],
+            function (err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({
+                        id: this.lastID,
+                        username,
+                        email,
+                        role: "user"
+                    });
+                }
+            }
+        );
+    });
+}
+
+async function registerUser(username, email, password) {
+    if (!username || !email || !password) {
+        return {
+            success: false,
+            statusCode: 400,
+            message: "Username, email and password are required."
+        }
+    }
+
+    if (username.trim().length < 3) {
+        return {
+            success: false,
+            statusCode: 400,
+            message: "Username must be at least 3 characters long."
+        }
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+        return {
+            success: false,
+            statusCode: 400,
+            message: "Invaild email format."
+        }
+    }
+
+    if (password.length < 8) {
+        return {
+            success: false,
+            statusCode: 400,
+            message: "Password must be at least 8 characters long."
+        }
+    }
+
+    const existingUser = await findExistingUser(username, email);
+
+    if (existingUser) {
+        return {
+            success: false,
+            statusCode: 409,
+            message: "Username or email already exists."
+        }
+    }
+
+    const saltRounds = 12;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const newUser = await insertUser(username, email, hashedPassword);
+
+    return {
+        success: true,
+        statusCode: 201,
+        message: "User registered successfully.",
+        user: newUser
+    }
+}
+
 async function loginUser(identifier, password) {
     if (!identifier || !password) {
         return {
@@ -74,4 +168,4 @@ async function loginUser(identifier, password) {
     }
 }
 
-module.exports = { loginUser };
+module.exports = { loginUser, registerUser };
