0a6637b48e54166604d957bcc09a0527be8e7c3b
mismetdemir <mismetdemir@hotmail.com>
2026-05-19 11:33:31 +0300

    Added JWT authorization method

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -18,4 +18,19 @@ function authenticateToken(req, res, next) {
     }
 }
 
-module.exports = authenticateToken;
+function authorizeRoles(...allowedRoles) {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.status(401).json{( message: "User is not authenticated." )};
+        }
+
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({ message: "You do not have permission to perform this action." });
+        }
+
+        next();
+    }
+}
+
+
+module.exports = { authenticateToken, authorizeRoles };
